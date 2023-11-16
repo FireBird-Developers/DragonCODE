8a4e232e3aff367ebd94e7c4272dc78905657934
JakubGawron1 <jakubgawron.dev.pl@gmail.com>
2023-11-16 08:46:04 +0100

    Create skieleton view

--- build.rs
+++ build.rs
@@ -1,5 +1,5 @@
 fn main(){
     let config = slint_build::CompilerConfiguration::new()
-        .with_style("material".into());
-    slint_build::compile_with_config(r"src\ui\MainWindow.slint").unwrap();
+        .with_style("material-dark".to_owned());
+    slint_build::compile_with_config(r"src\ui\MainWindow.slint", config).unwrap();
 }
--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,10 @@
 slint::include_modules!();
 
 fn main()-> Result<(), slint::PlatformError>{
-    let ui = MainWindow::new()?;
+    
 
+    let ui = MainWindow::new()?;
+    
+       
     ui.run()
 }
